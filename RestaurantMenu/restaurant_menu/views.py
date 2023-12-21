84214662c81cff9b69d96dcf90c6703eb14163bc
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-12-21 11:09:40 +0200

    Add MenuItemAdmin to admin.py and update menu_list.html template

--- RestaurantMenu/restaurant_menu/views.py
+++ RestaurantMenu/restaurant_menu/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import MenuItem
+from .models import MenuItem, MEAL_TYPE
 
 
 # Create your views here.
 class MenuList(generic.ListView):
     queryset = MenuItem.objects.order_by('date_created')
     template_name = 'menu_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['meals'] = MEAL_TYPE
+        return context
 
 
 class MenuItemDetail(generic.DetailView):
